fc33d176d008b1b279f77dc83daf0c27a35d07c0
JulianPaez92 <jepaez92@gmail.com>
2021-06-27 00:29:14 -0300

    Creación de archivos y de funciones en pais.

---
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Pais.kt
@@ -0,0 +1,21 @@
+package ar.edu.unahur.obj2.impostoresPaises
+
+class Pais (
+    val nombre: String,
+    val codigoISO: String,
+    val poblacion: Int,
+    val superficie: Double,
+    val continente: String,
+    val bloquesRegionales: MutableSet<String>,
+    val idiomas: MutableSet<String>
+        ){
+    var paisesLimitrofes = mutableSetOf<Pais>()
+
+    fun esPlurinacional() = idiomas.size > 1
+
+    fun esUnaIsla() = paisesLimitrofes.isEmpty()
+
+    fun densidadPoblacional() = poblacion / superficie
+
+    fun vecinoMasPoblado() = "Busca al limitrofe mas poblado, si es mas poblado que el que consulta va el limitrofe sino va el que consulta"
+}
